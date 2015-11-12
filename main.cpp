581984711c7513fc6f9520aad9000f28b5b18c69
Chris Beck <beck.ct@gmail.com>
2015-11-11 20:17:18 -0500

    replace the main.cpp exactly with utility_client.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
-// From websocketpp examples, called "utility client" there
+/*
+ * Copyright (c) 2014, Peter Thorson. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the WebSocket++ Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PETER THORSON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 // **NOTE:** This file is a snapshot of the WebSocket++ utility client tutorial.
 // Additional related material can be found in the tutorials/utility_client
@@ -239,68 +263,62 @@ int main() {
     std::string input;
     websocket_endpoint endpoint;
 
-    std::cout << "Welcome! Type 'help' to get a list of commands.\n" << std::endl;
-
     while (!done) {
         std::cout << "Enter Command: ";
         std::getline(std::cin, input);
 
-        try {
-          if (input == "quit") {
-              done = true;
-          } else if (input == "help") {
-              std::cout
-                  << "\nCommand List:\n"
-                  << "connect <ws uri>\n"
-                  << "send <connection id> <message>\n"
-                  << "close <connection id> [<close code:default=1000>] [<close reason>]\n"
-                  << "show <connection id>\n"
-                  << "help: Display this help text\n"
-                  << "quit: Exit the program\n"
-                  << std::endl;
-          } else if (input.substr(0,7) == "connect") {
-              int id = endpoint.connect(input.substr(8));
-              if (id != -1) {
-                  std::cout << "> Created connection with id " << id << std::endl;
-              }
-          } else if (input.substr(0,4) == "send") {
-              std::stringstream ss(input);
+        if (input == "quit") {
+            done = true;
+        } else if (input == "help") {
+            std::cout
+                << "\nCommand List:\n"
+                << "connect <ws uri>\n"
+                << "send <connection id> <message>\n"
+                << "close <connection id> [<close code:default=1000>] [<close reason>]\n"
+                << "show <connection id>\n"
+                << "help: Display this help text\n"
+                << "quit: Exit the program\n"
+                << std::endl;
+        } else if (input.substr(0,7) == "connect") {
+            int id = endpoint.connect(input.substr(8));
+            if (id != -1) {
+                std::cout << "> Created connection with id " << id << std::endl;
+            }
+        } else if (input.substr(0,4) == "send") {
+            std::stringstream ss(input);
             
-              std::string cmd;
-              int id;
-              std::string message = "";
+            std::string cmd;
+            int id;
+            std::string message = "";
             
-              ss >> cmd >> id;
-              std::getline(ss,message);
+            ss >> cmd >> id;
+            std::getline(ss,message);
             
-              endpoint.send(id, message);
-          } else if (input.substr(0,5) == "close") {
-              std::stringstream ss(input);
+            endpoint.send(id, message);
+        } else if (input.substr(0,5) == "close") {
+            std::stringstream ss(input);
             
-              std::string cmd;
-              int id;
-              int close_code = websocketpp::close::status::normal;
-              std::string reason = "";
+            std::string cmd;
+            int id;
+            int close_code = websocketpp::close::status::normal;
+            std::string reason = "";
             
-              ss >> cmd >> id >> close_code;
-              std::getline(ss,reason);
+            ss >> cmd >> id >> close_code;
+            std::getline(ss,reason);
             
-              endpoint.close(id, close_code, reason);
-          } else if (input.substr(0,4) == "show") {
-              int id = atoi(input.substr(5).c_str());
+            endpoint.close(id, close_code, reason);
+        } else if (input.substr(0,4) == "show") {
+            int id = atoi(input.substr(5).c_str());
 
-              connection_metadata::ptr metadata = endpoint.get_metadata(id);
-              if (metadata) {
-                  std::cout << *metadata << std::endl;
-              } else {
+            connection_metadata::ptr metadata = endpoint.get_metadata(id);
+            if (metadata) {
+                std::cout << *metadata << std::endl;
+            } else {
                 std::cout << "> Unknown connection id " << id << std::endl;
-              }
-          } else {
-              std::cout << "> Unrecognized Command" << std::endl;
-          }
-       } catch (std::out_of_range & e) {
-         std::cerr << "Caught 'out of range' exception: " << e.what() << std::endl;
-       }
+            }
+        } else {
+            std::cout << "> Unrecognized Command" << std::endl;
+        }
     }
 
     return 0;
